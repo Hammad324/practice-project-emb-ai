e20df501639e11707d9347cb5a91e834a99103c7
Hammad-Farrukh <hammadfarrukh602@gmail.com>
2023-08-30 20:27:39 +0500

    ,

---
+++ SentimentAnalysis/__init__.py
@@ -0,0 +1,1 @@
+from SentimentAnalysis import sentiment_analysis
---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,21 @@
+import requests, json
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    return {'label': label, 'score': score}
+
+
+"""
+After successful import, test your application with the text “I love this new technology.”
+sentiment_analyzer("I love this new technology.")'{"documentSentiment":{"score":0.996586, "label":"SENT_POSITIVE", "mixed":false, "sentimentMentions":[{"span":{"begin":0, "end":27, "text":"I love this new technology."}, "sentimentprob":{"positive":0.9934198, "neutral":0.0030947884, "negative":0.0034853641}}]}, "targetedSentiments":{"targetedSentiments":{}, "producerId":{"name":"Aggregated Sentiment Workflow", "version":"0.0.1"}}, "producerId":{"name":"Aggregated Sentiment Workflow", "version":"0.0.1"}}'
+"""
